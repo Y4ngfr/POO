725787c21b31102a6d5fafae7588584e6d7381ef
Y4ngfr <yangrodrigues.aluno@unipampa.edu.br>
2024-04-15 20:46:54 -0300

    tarefa 3

--- SistemaTransportadora/main.cpp
+++ SistemaTransportadora/main.cpp
@@ -26,18 +26,17 @@ int main()
     // Listas
     ListaVeiculos *veiculos;
     ListaVeiculos *veiculosDisponiveis;
-    ListaVeiculos *veiculosLocalizacao;
 
     // Inicializando Veículos
-    veiculo = new Veiculo("BRAOS17", "Carro", 100, 1992, "KSJ381NM4", "Fiat", "Brasil", 1);
-    caminhao = new Caminhao("376FF12", 139.54, 2001, "SJNC3823", "VUC", "França", 1, 2155.90, "6x4");
-    carro = new Carro("SBL4771", 43.2, 2013, "KIROP43L", "Fiat", "Uruguai", 0, "Gasolina", "80 CV");
-    moto = new Moto("IVX4H97", 10, 2013, "PCION389C", "Honda", "China", 1, "Flex", "300 cilindradas", "City");
-    moto2 = new Moto("IVX4H97", 10, 2013, "PCION389C", "Honda", "China", 1, "Flex", "300 cilindradas", "City");
+    veiculo = new Veiculo("BRAOS17", "Carro", 100, 1992, "KSJ381NM4", "Fiat", "24.08, 32.56", 1);
+    caminhao = new Caminhao("376FF12", 139.54, 2001, "SJNC3823", "VUC", "12.99, 14.15", 1, 2155.90, "6x4");
+    carro = new Carro("SBL4771", 43.2, 2013, "KIROP43L", "Fiat", "12.03, 13.4", 0, "Gasolina", "80 CV");
+    moto = new Moto("IVX4H97", 10, 2013, "PCION389C", "Honda", "24.67, 45.78", 1, "Flex", "300 cilindradas", "City");
+    moto2 = new Moto("IVX4H97", 10, 2013, "PCION389C", "Honda", "11.11, 11.12", 1, "Flex", "300 cilindradas", "City");
 
     // Inicializando Clientes e Pedidos
     cliente = new Cliente("9312045493", "Marcos", "0123456789", "123.456.789-10", 34);
-    pedido = new Pedido("9313204549", "Terreno", "Rua 12", "Rua 13", 21, 345);
+    pedido = new Pedido("9313204549", "Terreno", "13.14, 23.55", "45.32, 67.89", 21, 345);
 
     // Inicializando lista de veículos
     veiculos = new ListaVeiculos();
@@ -54,21 +53,17 @@ int main()
     veiculos->adicionarVeiculo(moto2, FINAL);
 
     veiculosDisponiveis = veiculos->obterVeiculosDisponiveis();
-    veiculosLocalizacao = veiculos->filtrarPorLocalizacao("Brasil");
     
     cout << "Veículos:" << endl;
     cout << *veiculos << endl;
     cout << "Veículos disponíveis:" << endl;
     cout << *veiculosDisponiveis << endl;
-    cout << "Veículos filtrados por localização:" << endl;
-    cout << *veiculosLocalizacao << endl;
 
     if(*(veiculos->buscarVeiculo(INICIO)) == *(veiculos->buscarVeiculo(FINAL))){
         cout << "são iguais" << endl;
     }
 
     delete veiculosDisponiveis;
-    delete veiculosLocalizacao;
     delete veiculos;
     delete veiculo;
     delete carro;
